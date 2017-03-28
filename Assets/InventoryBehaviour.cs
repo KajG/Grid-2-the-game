05685c6043231a173027fd9411134789709502d7
KajG <kaaiman99@hotmail.com>
2017-03-28 16:22:02 +0200

    oke

--- Assets/InventoryBehaviour.cs
+++ Assets/InventoryBehaviour.cs
@@ -7,6 +7,8 @@ public class InventoryBehaviour : MonoBehaviour {
 	public static Dictionary<string, int> _items = new Dictionary<string, int> ();
 	public Dictionary<string, int> _getItems{get{return _items;} set{_items = value;}}
 	public Raycasting raycasting;
+	private int j;
+	public GameObject _cube;
 
 	void Start () {
 		raycasting = raycasting.GetComponent<Raycasting> ();
@@ -26,9 +28,19 @@ public class InventoryBehaviour : MonoBehaviour {
 		if (_items [item] <= 0) {
 			_items.Remove (item);
 		} else {
+			for (int i = 0; i < amount; i++) {
+				Instantiate (_cube, new Vector3(j + 19,0,-8), Quaternion.identity);
+			}
 			_items [item] -= amount;
 		}
-		raycasting.GotItem (item, amount);
+	}
+	public void GotItem(string item, int amount){
+		if (item == itemname) {
+			for (int i = 0; i < amount; i++) {
+				j++;
+				Instantiate (_cube, new Vector3(j + 19,0,-8), Quaternion.identity);
+			}
+		}
 	}
 	void OnTriggerEnter(Collider other){
 		AddItem (other.gameObject.name, 1);
--- Assets/Raycasting.cs
+++ Assets/Raycasting.cs
@@ -9,6 +9,7 @@ public class Raycasting : MonoBehaviour {
 	public string itemname;
 	public int amountOfObjectsRetrieved;
 	public GameObject _cube;
+	private int j;
 	void Start(){
 		inventorybehaviour = inventorybehaviour.GetComponent<InventoryBehaviour> ();
 	}
@@ -17,16 +18,17 @@ public class Raycasting : MonoBehaviour {
 		mousePos.z = 10;
 		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		if (Physics.Raycast (ray, out hit) && hit.transform.tag == "Item" && Input.GetMouseButton(0)) {
+		if (Physics.Raycast (ray, out hit) && hit.transform.tag == "Item") {
 			hit.transform.position = mousePos;
-		} else if (Physics.Raycast (ray, out hit) && hit.transform.tag == "Inventory" && inventorybehaviour._getItems.Count > 0 && Input.GetMouseButton(0)){
+		} else if (Physics.Raycast (ray, out hit) && hit.transform.tag == "Inventory" && inventorybehaviour._getItems[itemname] > 0 && Input.GetMouseButtonDown(0)){
 			inventorybehaviour.GetItem (itemname, amountOfObjectsRetrieved);
 		}
 	}
 	public void GotItem(string item, int amount){
 		if (item == itemname) {
 			for (int i = 0; i < amount; i++) {
-				Instantiate (_cube, mousePos, Quaternion.identity);
+				j++;
+				Instantiate (_cube, new Vector3(j + 19,0,-8), Quaternion.identity);
 			}
 		}
 	}
