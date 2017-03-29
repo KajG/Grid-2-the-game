a1c559fdecbe094d3b8e369cbe5ab7174688908b
KajG <kaaiman99@hotmail.com>
2017-03-29 14:55:42 +0200

    changes

--- Assets/InventoryBehaviour.cs
+++ Assets/InventoryBehaviour.cs
@@ -4,46 +4,46 @@ using UnityEngine;
 
 public class InventoryBehaviour : MonoBehaviour {
 
-	public static Dictionary<string, int> _items = new Dictionary<string, int> ();
-	public Dictionary<string, int> _getItems{get{return _items;} set{_items = value;}}
+	public static Dictionary<string, List<GameObject>> _items = new Dictionary<string, List<GameObject>> ();
+	public Dictionary<string, List<GameObject>> _getItems{get{return _items;} set{_items = value;}}
+	public List <GameObject> _objects = new List<GameObject>();
 	public Raycasting raycasting;
 	private int j;
 	public GameObject _cube;
+	public string itemname;
 
 	void Start () {
 		raycasting = raycasting.GetComponent<Raycasting> ();
 	}
 	void Update(){
-		//print (_items["Cube(clone)"]);
+		
 	}
-	public void AddItem(string item, int amount){
-		if (_items.ContainsKey (item)) {
-			_items [item]++; 
+	public void AddItem(string itemname, GameObject obj){
+		if (_items.ContainsKey (itemname)) {
+			_items[itemname].Add(obj); 
+			print (_items[itemname].Count);
+
 		} else {
-			_items.Add (item, amount);
-		}
-		print (_items["Cube(Clone)"]);
-	}
-	public void GetItem(string item, int amount){
-		if (_items [item] <= 0) {
-			_items.Remove (item);
-		} else {
-			for (int i = 0; i < amount; i++) {
-				Instantiate (_cube, new Vector3(j + 19,0,-8), Quaternion.identity);
-			}
-			_items [item] -= amount;
+			_items.Add(itemname, new List<GameObject>());
+			_items[itemname].Add(obj); 
+			print (_items[itemname].Count);
 		}
 	}
-	public void GotItem(string item, int amount){
-		if (item == itemname) {
+	public void GetItem(string itemname, int amount){
+		if (_items [itemname].Count <= 0) {
+			_items.Remove (itemname);
+		} else {
 			for (int i = 0; i < amount; i++) {
 				j++;
-				Instantiate (_cube, new Vector3(j + 19,0,-8), Quaternion.identity);
+				GameObject obj = Instantiate (_items [itemname][0], new Vector3 (j + 19, 0, -8), Quaternion.identity);
+				obj.name = (itemname);
+				Destroy (_items [itemname] [0]);
+				_items [itemname].RemoveAt (0);
 			}
 		}
 	}
 	void OnTriggerEnter(Collider other){
-		AddItem (other.gameObject.name, 1);
-		Destroy (other.gameObject);
+		AddItem (other.gameObject.name, other.gameObject);
+
 	}
 }
