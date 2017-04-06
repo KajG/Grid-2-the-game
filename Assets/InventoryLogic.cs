dbff3aa51ddb92018807fbb4af156a7fea3202b0
KajG <kaaiman99@hotmail.com>
2017-04-06 08:59:08 +0200

    Het werkt.

--- Assets/InventoryLogic.cs
+++ Assets/InventoryLogic.cs
@@ -7,12 +7,19 @@ using UnityEngine.UI;
 public class InventoryLogic : MonoBehaviour {
 
 	public List<Image> _slotImage = new List<Image> ();
+	public List<Button> _buttonSlots = new List<Button> ();
 	public List<List<GameObject>> _slots = new List<List<GameObject>>();
 	public int _index;
 	public int _openSlotIndex;
 	public int _slotCount;
+	public int spawnXOffset;
+	public int spawnYOffset;
 	public bool itemAdded;
-	public List<int> _openSlots = new List<int>();
+	public string _itemName;
+	public Sprite _placeHolder;
+	public List<int> _openSlots = new List<int> ();
+	public List<int> _usedSlots = new List<int> ();
+	public List<int> _sameObj = new List<int> ();
 	void Start () {
 		for (int i = 0; i < _slotCount; i++) {
 			_slots.Add (new List<GameObject> ());
@@ -20,18 +27,52 @@ public class InventoryLogic : MonoBehaviour {
 	}
 	
 	void Update () {
-		print (_slots[0].Count);
+		print (_slots[2].Count);
+	}
+	void CheckSlots(GameObject item){
+		_sameObj.Clear();
+		_usedSlots.Clear ();
+		_openSlots.Clear ();
+		_itemName = item.name;
+		for (int i = 0; i < _slots.Count; i++) {
+			if(_slots[i].Count > 0){
+				if (_slots [i][0].name == _itemName) {
+					_sameObj.Add (i);
+				}
+			} else if(_slots[i].Count <= 0) {
+				_openSlots.Add (i);
+			}
+			if (_slots [i].Count > 0 && _slots[i][0].name == _itemName) {
+				_usedSlots.Add (i);
+			} 
+		}
+		AddItem (item);
 	}
 	void AddItem(GameObject item){
-
+		if (_sameObj.Count > 0) {
+			_slots [_sameObj [0]].Add (item);
+		} else {
+			_slots [_openSlots [0]].Add (item);
+			_slotImage [_openSlots [0]].sprite = item.GetComponent<Image> ().sprite;
+		}
+	}
+	public void OnButtonPress(int index){
+		if (_slots [index].Count <= 0) {
+			return;
+		}
+		_slots [index] [0].SetActive (true);
+		GameObject obj = Instantiate (_slots [index][0], new Vector3(transform.position.x + spawnXOffset, transform.position.y + spawnYOffset, transform.position.z), Quaternion.identity);
+		obj.name = _slots [index] [0].name;
+		Destroy (_slots [index][0]);
+		_slots [index].RemoveAt (0);
+		if (_slots [index].Count <= 0) {
+			_slotImage [index].sprite = _placeHolder;
+		}
 	}
 	void OnTriggerEnter(Collider item){
 		if (item.gameObject.tag == "Item") {
-			AddItem (item.gameObject);		
+			CheckSlots (item.gameObject);	
+			item.gameObject.SetActive (false);
 		}
 	}
 }
-/*if(_slots [_index].Contains (item)){
-	ExistingItem(item);
-	return;
-}*/
---
+++ Assets/TestSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawn : MonoBehaviour {
+
+	public GameObject item;
+	public GameObject item2;
+	public GameObject item3;
+	public int spawnAmount;
+
+	void Start () {
+		for (int i = 0; i < spawnAmount; i++) {
+			int randNumb = Random.Range (0, 4);
+			if (randNumb == 1) {
+				Instantiate (item, new Vector3 (i, 0, 0), Quaternion.identity);
+			} else if (randNumb == 2) {
+				Instantiate (item2, new Vector3 (0, 0, i), Quaternion.identity);
+			} else {
+				Instantiate (item3, new Vector3 (i, 0, i), Quaternion.identity);
+			}
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
